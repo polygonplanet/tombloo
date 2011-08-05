49ee6e37a9e5051a218765e8877157a28b923d4f
polygonplanet <polygon.planet@gmail.com>
2011-08-06 04:18:59 +0900

    「アカウントの切り替え」ダイアログを内容サイズに合わせてリサイズするパッチ。 ソートが動いてなかったので修正。 リサイズが固定だったので適切になるよう修正。

--- tombloo.service.actions.changeacount.resize.js
+++ tombloo.service.actions.changeacount.resize.js
@@ -1,7 +1,7 @@
 /**
  * Service.Actions.ChangeAcount.Resize - Tombloo patches
  *
- * 「アカウントの切り替え」ダイアログをリサイズするだけのパッチ
+ * 「アカウントの切り替え」ダイアログを内容サイズに合わせてリサイズするパッチ
  *
  * 機能:
  * --------------------------------------------------------------------------
@@ -12,8 +12,8 @@
  *
  * --------------------------------------------------------------------------
  *
- * @version    1.03
- * @date       2011-08-05
+ * @version    1.04
+ * @date       2011-08-06
  * @author     polygon planet <polygon.planet@gmail.com>
  *              - Blog    : http://polygon-planet.blogspot.com/
  *              - Twitter : http://twitter.com/polygon_planet
@@ -34,96 +34,119 @@ const MAX_HEIGHT = 600;
 // changeAcountをアップデート
 update(Tombloo.Service.actions[getMessage('label.action.changeAcount')], {
     execute: function() {
-        let win, resize, start, timeout, interval;
-        start    = +new Date;
-        timeout  = 15 * 1000;
-        interval = 150;
+        let win, resize, sort, getItems, usersList, modelSelect;
+        getItems = function() {
+            let items = win.document.querySelectorAll('#users .listitem-iconic');
+            return Array.prototype.slice.call(items) || [];
+        };
         resize = function() {
-            let elems, stop = false, size = {}, max = {
-                diff   : 0,
-                height : 0
-            };
-            // リストアイテムすべての幅を調べる (読み込まれてる場合)
-            elems = win.document.querySelectorAll('#users .listitem-iconic');
-            elems = Array.prototype.slice.call(elems) || [];
-            elems.forEach(function(item) {
-                let diff, orgWidth;
-                try {
-                    orgWidth = item.width;
-                    item.style.overflow = 'scroll';
-                    diff = item.scrollWidth - orgWidth;
-                    max.height += item.scrollHeight || 0;
-                    if (diff > max.diff) {
-                        max.diff = diff;
-                    }
-                } catch (e) {} finally {
-                    if (item) {
-                        item.style.overflow = '';
-                    }
-                }
-            });
-            if (max.diff > 0) {
-                try {
-                    size.width  = win.outerWidth + max.diff + 60;
-                    size.height = win.outerHeight + 10;
-                    win.resizeTo(
-                        Math.min(MAX_WIDTH, size.width)   || MAX_WIDTH,
-                        Math.min(MAX_HEIGHT, size.height) || MAX_HEIGHT
-                    );
-                    stop = true;
-                    win.centerWindowOnScreen();
-                } catch (e) {}
-            }
-            // ロードが完了してない場合はタイムアウトまで再試行
-            if (!stop && (+new Date - start < timeout)) {
-                setTimeout(resize, interval);
-            }
-            if (stop) {
-                // ユーザー名でソート
-                callLater(1, function() {
-                    let listbox, sortItems = function() {
-                        let newItems = [], labels = [];
-                        elems.forEach(function(item) {
-                            labels.push(item.getAttribute('label'));
-                        });
-                        alphanumSort(labels);
-                        labels.forEach(function(label) {
-                            let done = false;
-                            elems.forEach(function(item) {
-                                if (!done && item.getAttribute('label') === label) {
-                                    newItems.push(item);
-                                    done = true;
-                                }
-                            });
-                        });
-                        if (newItems && newItems.length === elems.length) {
-                            elems.forEach(function(item) {
-                                if (!listbox) {
-                                    listbox = item.parentNode;
-                                }
-                                removeElement(item);
-                            });
-                            newItems.forEach(function(item) {
-                                listbox.appendChild(item);
-                            });
+            let d = new Deferred(), start, timeout, interval, resizeTo;
+            start    = +new Date;
+            timeout  = 15 * 1000;
+            interval = 150;
+            resizeTo = function() {
+                let elems, stop = false, size = {}, max;
+                max = {
+                    diff   : 0,
+                    height : 0,
+                    box    : usersList.boxObject.width
+                };
+                // リストアイテムすべての幅を調べる (読み込まれてる場合)
+                getItems().forEach(function(item) {
+                    let diff;
+                    try {
+                        diff = Math.floor((item.getAttribute('label').length * 6.128) - max.box);
+                        max.height += item.scrollHeight || 0;
+                        if (diff > max.diff) {
+                            max.diff = diff;
                         }
-                    };
-                    callLater(0, function() {
-                        sortItems();
-                    });
+                    } catch (e) {}
                 });
+                if (max.diff > 0) {
+                    try {
+                        size.width  = win.outerWidth + max.diff + 60;
+                        size.height = win.outerHeight + max.height + 20;
+                        win.resizeTo(
+                            Math.min(MAX_WIDTH, size.width)   || MAX_WIDTH,
+                            Math.min(MAX_HEIGHT, size.height) || MAX_HEIGHT
+                        );
+                        stop = true;
+                        win.centerWindowOnScreen();
+                    } catch (e) {}
+                }
+                // ロードが完了してない場合はタイムアウトまで再試行
+                if (!stop && (+new Date - start < timeout)) {
+                    setTimeout(resizeTo, interval);
+                } else {
+                    d.callback();
+                }
+            };
+            setTimeout(resizeTo, 0);
+            return d;
+        };
+        sort = function() {
+            let sorted = false, elems, newItems = [], labels = [];
+            elems = getItems();
+            elems.forEach(function(item) {
+                labels.push(item.getAttribute('label'));
+            });
+            alphanumSort(labels);
+            labels.forEach(function(label) {
+                let done = false;
+                elems.forEach(function(item) {
+                    if (!done && item.getAttribute('label') === label) {
+                        newItems.push(item);
+                        done = true;
+                    }
+                });
+            });
+            if (newItems && newItems.length === elems.length) {
+                clearChildren(usersList);
+                newItems.forEach(function(item) {
+                    usersList.appendChild(item);
+                    if (item.disabled || /tick[.]\w+$/i.test(item.getAttribute('image'))) {
+                        usersList.selectedItem = item;
+                    }
+                });
+                sorted = true;
             }
+            return sorted;
         };
         win = openDialog(
             'chrome://tombloo/content/library/login.xul',
             [
                 'resizable',
                 'centerscreen',
-                'width='  + MAX_WIDTH,
-                'height=' + MAX_HEIGHT
+                //'width='  + MAX_WIDTH,
+                //'height=' + MAX_HEIGHT
             ].join(',')
         );
-        setTimeout(resize, 0);
+        win.addEventListener('load', function() {
+            let doSort = function() {
+                let timeout = 5 * 1000, start = +new Date;
+                till(function() {
+                    let end = false;
+                    if (sort()) {
+                        end = true;
+                    } else if (+new Date - start > timeout) {
+                        end = true;
+                    }
+                    return end;
+                });
+            };
+            usersList = win.document.getElementById('users');
+            modelSelect = win.document.getElementById('models');
+            callLater(1, function() {
+                modelSelect.addEventListener('select', function() {
+                    doSort();
+                }, true);
+            });
+            callLater(0.12, function() {
+                resize().addCallback(function() {
+                    doSort();
+                });
+            });
+        }, true);
         return win;
     }
 });
@@ -137,9 +160,8 @@ update(Tombloo.Service.actions[getMessage('label.action.changeAcount')], {
  * @example   alphanumSort(['a10', 'a2', 'a100', 'a1', 'a12']);
  * @results   ['a1', 'a2', 'a10', 'a12', 'a100']
  *
- * @param  {Array}    array            対象の配列
- * @param  {Boolean}  caseInsensitive  大文字小文字を区別しない
- * @return {Array}                     ソートされた配列 (引数そのもの)
+ * @param  {Array}    array   対象の配列
+ * @return {Array}            ソートされた配列 (引数そのもの)
  */
 function alphanumSort(array) {
     let chunkify, alphanumCase;
