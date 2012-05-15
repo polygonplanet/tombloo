4c4dc55b7c6d6704b8e56cdc8c601ae04395b980
polygonplanet <polygon.planet.aqua@gmail.com>
2012-05-16 03:19:44 +0900

    Twitterのトークン取得URLを新しいものに変えるTomblooパッチ。

---
+++ tombloo.model.twitter.fixtoken.js
@@ -0,0 +1,39 @@
+/**
+ * Model.Twitetr.FixToken - Tombloo patches
+ *
+ * Twitterのトークン取得URLを新しいものに変えるTomblooパッチ
+ *
+ * 機能:
+ * --------------------------------------------------------------------------
+ *
+ * - Twitterのトークン取得URLをFix
+ *
+ * --------------------------------------------------------------------------
+ *
+ * @version    1.00
+ * @date       2012-05-16
+ * @author     polygon planet <polygon.planet.aqua@gmail.com>
+ *              - Twitter : http://twitter.com/polygon_planet
+ * @license    Same as Tombloo
+ * @updateURL  https://github.com/polygonplanet/tombloo/raw/master/tombloo.model.twitpic.fixicon.js
+ *
+ * Tombloo: https://github.com/to/tombloo/wiki
+ */
+(function() {
+
+update(Twitter, {
+    getToken : function() {
+        return request(this.URL + '/settings/account').addCallback(function(res) {
+            let html = res.responseText;
+            if (~html.indexOf('class="signin"')) {
+                throw new Error(getMessage('error.notLoggedin'));
+            }
+            return {
+                authenticity_token : html.extract(/authenticity_token.+value="(.+?)"/),
+                siv                : html.extract(/logout\?siv=(.+?)"/)
+            };
+        });
+    }
+});
+
+}());
