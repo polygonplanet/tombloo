a4a9e1d9ff435aa89d8a2e2ad5381439b8683520
polygonplanet <polygon.planet.aqua@gmail.com>
2012-09-20 15:20:41 +0900

    Tumblrでリブログできないのを修正するTomblooパッチ

---
+++ tombloo.extractor.tumblr.reblog.fix.js
@@ -0,0 +1,37 @@
+/**
+ * Extractor Tumblr Reblog Fix - Tombloo patches
+ *
+ * Tumblrでリブログできないのを修正するTomblooパッチ
+ *
+ * 機能:
+ * -----------------------------------------------------------------------
+ *
+ * - リブログ時のエラー修正
+ *
+ * -----------------------------------------------------------------------
+ *
+ * @version    1.00
+ * @date       2012-09-20
+ * @author     polygon planet <polygon.planet.aqua@gmail.com>
+ *              - Blog    : http://polygon-planet-log.blogspot.com/
+ *              - Twitter : http://twitter.com/polygon_planet
+ *              - Tumblr  : http://polygonplanet.tumblr.com/
+ * @license    Same as Tombloo
+ * @updateURL  https://github.com/polygonplanet/tombloo/raw/master/tombloo.extractor.tumblr.reblog.fix.js
+ *
+ * Tombloo: https://github.com/to/tombloo/wiki
+ */
+(function() {
+
+var orgMethod = Tombloo.Service.extractors.ReBlog.getFrameUrl;
+
+update(Tombloo.Service.extractors.ReBlog, {
+    getFrameUrl : function(doc) {
+        return $x(
+            '//iframe[starts-with(@src, "http://www.tumblr.com/iframe") and contains(@src, "pid=")]/@src',
+            doc
+        ) || orgMethod(doc);
+    }
+});
+
+}());
