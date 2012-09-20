7b9016eee6f81848ef8fed008fd709be66541408
polygonplanet <polygon.planet.aqua@gmail.com>
2012-09-21 01:04:44 +0900

    Tumblrでリブログできないのを修正するTomblooパッチ  - Update XPath.

--- tombloo.extractor.tumblr.reblog.fix.js
+++ tombloo.extractor.tumblr.reblog.fix.js
@@ -10,7 +10,7 @@
  *
  * -----------------------------------------------------------------------
  *
- * @version    1.02
+ * @version    1.03
  * @date       2012-09-21
  * @author     polygon planet <polygon.planet.aqua@gmail.com>
  *              - Blog    : http://polygon-planet-log.blogspot.com/
@@ -28,7 +28,9 @@ var orgMethod = Tombloo.Service.extractors.ReBlog.getFrameUrl;
 update(Tombloo.Service.extractors.ReBlog, {
     getFrameUrl : function(doc) {
         return $x(
-            '//iframe[contains(@src, ".tumblr.com/iframe") and contains(@src, "pid=") and contains(@src, "rk=")]/@src',
+            '//iframe[(starts-with(@src, "http://www.tumblr.com/iframe") or ' +
+                      'starts-with(@src, "http://assets.tumblr.com/iframe")) and ' +
+                      'contains(@src, "pid=")]/@src',
             doc
         ) || orgMethod(doc);
     }
