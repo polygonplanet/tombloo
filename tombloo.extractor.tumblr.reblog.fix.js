2c16b957855c4f5d93c2caadac201b53a25032fc
polygonplanet <polygon.planet.aqua@gmail.com>
2013-05-08 23:06:20 +0900

    Tumblrでリブログできないのを修正するTomblooパッチ  - Fixed "s is undefined".

--- tombloo.extractor.tumblr.reblog.fix.js
+++ tombloo.extractor.tumblr.reblog.fix.js
@@ -10,8 +10,8 @@
  *
  * -----------------------------------------------------------------------
  *
- * @version    1.03
- * @date       2012-09-21
+ * @version    1.10
+ * @date       2013-05-08
  * @author     polygon planet <polygon.planet.aqua@gmail.com>
  *              - Blog    : http://polygon-planet-log.blogspot.com/
  *              - Twitter : http://twitter.com/polygon_planet
@@ -21,19 +21,8 @@
  *
  * Tombloo: https://github.com/to/tombloo/wiki
  */
-(function() {
-
-var orgMethod = Tombloo.Service.extractors.ReBlog.getFrameUrl;
-
-update(Tombloo.Service.extractors.ReBlog, {
-    getFrameUrl : function(doc) {
-        return $x(
-            '//iframe[(starts-with(@src, "http://www.tumblr.com/iframe") or ' +
-                      'starts-with(@src, "http://assets.tumblr.com/iframe")) and ' +
-                      'contains(@src, "pid=")]/@src',
-            doc
-        ) || orgMethod(doc);
-    }
+addAround(Tombloo.Service.extractors['ReBlog'], 'getFrameUrl', function(proceed, args) {
+    var doc = args[0];
+    var re = /<iframe\s+src\s*=\s*(["']|)(http:\/\/assets\.tumblr\.com\/iframe.*?)\1/;
+    return proceed(args) || doc.documentElement.innerHTML.extract(re, 2);
 });
-
-}());
