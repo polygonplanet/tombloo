ec1716b0cdbcc2a3098e5827faa22b9f2f072283
polygonplanet <polygon.planet.aqua@gmail.com>
2013-05-10 13:06:29 +0900

    Tumblrでリブログできないのを修正するTomblooパッチ  - URLが取得できない不具合修正

--- tombloo.extractor.tumblr.reblog.fix.js
+++ tombloo.extractor.tumblr.reblog.fix.js
@@ -10,8 +10,8 @@
  *
  * -----------------------------------------------------------------------
  *
- * @version    1.10
- * @date       2013-05-08
+ * @version    1.11
+ * @date       2013-05-10
  * @author     polygon planet <polygon.planet.aqua@gmail.com>
  *              - Blog    : http://polygon-planet-log.blogspot.com/
  *              - Twitter : http://twitter.com/polygon_planet
@@ -23,6 +23,7 @@
  */
 addAround(Tombloo.Service.extractors['ReBlog'], 'getFrameUrl', function(proceed, args) {
     var doc = args[0];
-    var re = /<iframe\s+src\s*=\s*(["']|)(http:\/\/assets\.tumblr\.com\/iframe.*?)\1/;
-    return proceed(args) || doc.documentElement.innerHTML.extract(re, 2);
+    var re = /(?:<|\\x3c)iframe\b[\s\S]*?src\s*=\s*(["']|\\x22)(http:\/\/assets\.tumblr\.com\/.*?iframe.*?)\1/i;
+    var url = proceed(args) || doc.documentElement.innerHTML.extract(re, 2);
+    return (url || '').replace(/\\x22/g, '"').replace(/\\x26/g, '&');
 });
