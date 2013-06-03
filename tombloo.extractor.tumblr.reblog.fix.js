1f04510e86779a14edc1c9b9d4103b280c7b7425
polygonplanet <polygon.planet.aqua@gmail.com>
2013-06-03 12:46:45 +0900

    Tumblrでリブログできないのを修正するTomblooパッチ  - ps.type が取得できない不具合修正

--- tombloo.extractor.tumblr.reblog.fix.js
+++ tombloo.extractor.tumblr.reblog.fix.js
@@ -10,20 +10,47 @@
  *
  * -----------------------------------------------------------------------
  *
- * @version    1.11
- * @date       2013-05-10
+ * @version    1.12
+ * @date       2013-06-03
  * @author     polygon planet <polygon.planet.aqua@gmail.com>
- *              - Blog    : http://polygon-planet-log.blogspot.com/
  *              - Twitter : http://twitter.com/polygon_planet
- *              - Tumblr  : http://polygonplanet.tumblr.com/
  * @license    Same as Tombloo
  * @updateURL  https://github.com/polygonplanet/tombloo/raw/master/tombloo.extractor.tumblr.reblog.fix.js
  *
  * Tombloo: https://github.com/to/tombloo/wiki
  */
+(function() {
+
 addAround(Tombloo.Service.extractors['ReBlog'], 'getFrameUrl', function(proceed, args) {
     var doc = args[0];
     var re = /(?:<|\\x3c)iframe\b[\s\S]*?src\s*=\s*(["']|\\x22)(http:\/\/assets\.tumblr\.com\/.*?iframe.*?)\1/i;
     var url = proceed(args) || doc.documentElement.innerHTML.extract(re, 2);
     return (url || '').replace(/\\x22/g, '"').replace(/\\x26/g, '&');
 });
+
+
+update(Tumblr, {
+    getForm : function(url) {
+        var self = this;
+        return request(url).addCallback(function(res) {
+            var doc = convertToHTMLDocument(res.responseText);
+            var form = formContents(doc);
+            if (!form['post[type]']) {
+                form['post[type]'] = $x('//*[@id="header"]//select/option[@selected]/text()', doc).trim().toLowerCase();
+            }
+            delete form.preview_post;
+            form.redirect_to = Tumblr.TUMBLR_URL+'dashboard';
+            if(form.reblog_post_id){
+                self.trimReblogInfo(form);
+                // Tumblrから他サービスへポストするため画像URLを取得しておく
+                if(form['post[type]']=='photo')
+                    form.image = $x('id("edit_post")//img[contains(@src, "media.tumblr.com/") or contains(@src, "data.tumblr.com/")]/@src', doc);
+            }
+            return form;
+        });
+    },
+});
+
+
+}());
+
