1222e5b75da35b5f2bc31d63964b4432d3cfa1b4
polygonplanet <polygon.planet.aqua@gmail.com>
2013-09-19 03:07:39 +0900

    TumblrでhighRes画像を取得するTombfixパッチ  - Tumblr Photosetパッチに対応

--- tombloo.extractor.tumblr.highres.js
+++ tombloo.extractor.tumblr.highres.js
@@ -1,13 +1,13 @@
 /**
- * Extractor Tumblr HighRes - Tombloo patch
+ * Extractor Tumblr HighRes - Tombfix patch
  *
- * TumblrでhighRes画像を取得するTomblooパッチ
+ * TumblrでhighRes画像を取得するTombfixパッチ
  *
  * @version    1.01
- * @date       2013-06-15
+ * @date       2013-09-19
  * @author     polygon planet <polygon.planet.aqua@gmail.com>
- *              - Twitter: http://twitter.com/polygon_planet
- * @license    Same as Tombloo
+ * @link       http://twitter.com/polygon_planet
+ * @license    Public Domain
  * @updateURL  https://github.com/polygonplanet/tombloo/raw/master/tombloo.extractor.tumblr.highres.js
  *
  * Tombloo: https://github.com/to/tombloo/wiki
@@ -15,33 +15,48 @@
 (function() {
 
 addAround(Tombloo.Service.extractors['ReBlog'], 'extractByEndpoint', function(proceed, args) {
-    return proceed(args).addCallback(function(ps) {
+
+    function getHighRes(ps) {
         var itemUrl = ps.itemUrl;
 
         if (ps.type !== 'photo' || !/tumblr.*_\d+\.\w+$/.test(itemUrl)) {
-            return ps;
+            return succeed(ps);
         }
-
         var highRes = itemUrl.replace(/^(.*)_\d+\.(\w+)$/, '$1_1280.$2');
 
         if (highRes === itemUrl) {
-            return ps;
+            return succeed(ps);
         }
-
         return request(highRes).addErrback(function(err) {
             return ps;
         }).addCallback(function(res) {
             if (res.status == 200) {
-                itemUrl = highRes;
                 update(ps, {
-                    itemUrl : itemUrl,
+                    itemUrl : highRes,
                     form    : {
-                        image : itemUrl
+                        image : highRes
                     }
                 });
             }
             return ps;
         });
+    }
+
+    return proceed(args).addCallback(function(ps) {
+        if (ps.favorite && ps.favorite.form &&
+            Array.isArray(ps.favorite.form.images) && ps.favorite.form.images.length
+        ) {
+            // 画像が複数 (Photoset)
+            return deferredForEach(ps.favorite.form.images, function(image, i) {
+                return wait(0).addCallback(function() {
+                    return getHighRes(update({}, ps, { itemUrl : image })).addCallback(function(nps) {
+                        ps.favorite.form.images[i] = nps.itemUrl;
+                        return ps;
+                    });
+                });
+            });
+        }
+        return getHighRes(ps);
     });
 });
 
